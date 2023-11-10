c0db95e596d2ae41fd2b38bdecaf6e01bd0aaf37
ibertagnolli <ibertagnolli@hotmail.com>
2023-11-10 08:00:18 -0700

    Updated padding for sleep page.

--- lib/app/pages/tracking/sleep.dart
+++ lib/app/pages/tracking/sleep.dart
@@ -10,17 +10,17 @@ class SleepPage extends StatefulWidget {
 
 class _SleepPageState extends State<SleepPage> {
   //final stopWatch = Stopwatch();
-  String timerData = "0:00";
+  String timerData = "00:00:00";
   String buttonText = "Start Nap";
-  bool buttonToggled = false; 
+  bool buttonToggled = false;
 
   void napClicked() {
     setState(() {
       // timerData = stopWatch.elapsedMilliseconds;
       // print(stopWatch.elapsedMilliseconds); // 0
       // stopWatch.start();
-      buttonToggled = true; 
-      timerData = "0:01";
+      buttonToggled = true;
+      timerData = "00:00:01";
       buttonText = "Stop Nap";
     });
   }
@@ -30,21 +30,35 @@ class _SleepPageState extends State<SleepPage> {
     return MaterialApp(
       title: 'Sleep',
       home: Scaffold(
-      backgroundColor: const Color(0xffb3beb6),
+        backgroundColor: const Color(0xffb3beb6),
         appBar: AppBar(
-          title: const Text('Tracking', style: TextStyle(fontSize: 36, color: Colors.black45)),
-           backgroundColor: Color(0xFFFFFAF1),
+          title: const Text('Tracking',
+              style: TextStyle(fontSize: 36, color: Colors.black45)),
+          backgroundColor: Color(0xFFFFFAF1),
         ),
         body: Center(
           child: Column(children: <Widget>[
-            const Text('Sleep', style: TextStyle(fontSize: 36, color: Color(0xFFFFFAF1))),
-            const FilledCard(),
-            Text("$timerData", style: const TextStyle(fontSize: 20, color: Color(0xFFFFFAF1))),
-            FilledButton(
-               style: FilledButton.styleFrom( backgroundColor:const Color.fromARGB(255, 13, 60, 70), // Background color
-  ),
-              onPressed: napClicked,
-              child: Text("$buttonText",  style: const TextStyle(fontSize: 20, color: Color(0xFFFFFAF1)))
+            const Padding(
+              padding: EdgeInsets.all(32),
+              child: Text('Sleep',
+                  style: TextStyle(fontSize: 36, color: Color(0xFFFFFAF1))),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 16),
+              child: const FilledCard(),
+            ),
+            Text("$timerData", style: const TextStyle(fontSize: 30, color: Color(0xFFFFFAF1))),
+             Padding(
+              padding: EdgeInsets.all(32),
+            child:FilledButton(
+                style: FilledButton.styleFrom(
+                  backgroundColor:
+                       Color.fromARGB(255, 13, 60, 70), // Background color
+                ),
+                onPressed: napClicked,
+                child: Text("$buttonText",
+                    style: const TextStyle(
+                        fontSize: 20, color: Color(0xFFFFFAF1)))),
             ),
           ]),
         ),
@@ -63,7 +77,7 @@ class FilledCard extends StatelessWidget {
       child: Card(
         elevation: 0,
         color: Color(0xFFFFFAF1),
-        child:  SizedBox(
+        child: SizedBox(
           width: 300,
           height: 100,
           child: Column(children: <Widget>[
@@ -73,7 +87,7 @@ class FilledCard extends StatelessWidget {
             ),
             Divider(height: 0),
             ListTile(
-              title: Text('Last Nap Length: 0:55'),
+              title: Text('Last Nap Length: 00:55'),
               // tileColor: ,
             ),
             Divider(height: 0),
