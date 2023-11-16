246ea8ac32375d21f4e593fd8bf8f54a9ab0b8ee
ibertagnolli <ibertagnolli@hotmail.com>
2023-11-15 17:40:38 -0700

    Basic calendar page

--- lib/app/pages/calendar.dart
+++ lib/app/pages/calendar.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:babysteps/app/widgets/widgets.dart';
 import 'package:babysteps/app/widgets/checkList.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'dart:core';
 
-
-void main() => runApp(const CalendarPage());
+//TODO: add navigation to this page from any page.
+//This next line allows me to run the calendar page as main since we don't have the navigation to the calendar page setup yet.
+//void main() => runApp(const CalendarPage());
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -14,13 +15,11 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
- static const  String item1 = "3";
+  //TODO: propagate these item strings through to the checklist box
+  static const String item1 = "3";
   static const String item2 = "101.5";
   static const String item3 = "do dishes";
   String buttonText = "Add Temp";
- 
-
- 
 
   @override
   Widget build(BuildContext context) {
@@ -45,48 +44,21 @@ class _CalendarPageState extends State<CalendarPage> {
                   style: TextStyle(fontSize: 36, color: Color(0xFFFFFAF1))),
             ),
 
-            // FilledCard Quick Weight Info
-            // Padding(
-            //   padding: EdgeInsets.only(bottom: 15),
-            //   child: FilledCard("last temperature: $daysSinceTemp",
-            //       "temperature: $lastTemp", Icon(Icons.device_thermostat)),
-            // ),
+            // Very Very Basic calendar 
+            //TODO: use api to flesh out calendar and make a more interactive calendar
+            Padding(
+              padding: EdgeInsets.only(bottom: 15),
+              child: TableCalendar(
+                firstDay: DateTime.utc(2023, 10, 16),
+                lastDay: DateTime.utc(2025, 3, 14),
+                focusedDay: DateTime.now(),
+              ),
+            ),
 
-            // Add temp Card // TODO: round corners, default card open, turn into widget
-          //  const Padding(
-          //     padding: const EdgeInsets.all(15),
-          //     child: ExpansionTile(
-          //       backgroundColor: const Color(0xFFFFFAF1),
-          //       collapsedBackgroundColor: const Color(0xFFFFFAF1),
-          //       title:  Text('TO DO:',
-          //           style: TextStyle(
-          //               fontSize: 20,
-          //               color: Colors.black,
-          //               fontWeight: FontWeight.bold)),
-          //       children: <Widget>[
-          //          Text('Get Groceries',
-          //             style: TextStyle(fontSize: 20, color: Colors.black)),
-          //         Padding(
-          //           padding: EdgeInsets.all(20),
-                  
-          //         ),
-          //         Column(
-          //           children: <Widget>[
-          //              Padding(
-          //               padding: EdgeInsets.only(left: 20),
-          //               //put check list here
-          //                child: CheckboxListTileApp()
-          //             ),
-                      
-          //           ],
-          //         ),
-          //         Padding(padding: EdgeInsets.only(bottom: 20)),
-                  
-          //       ],
-          //     ),
-          //   ),
-
-  const Padding(
+            //To Do list card 
+            //TODO: propogate todo items from variables through to the widget
+            //TODO: add notes icon and integration at bottom of card 
+            const Padding(
               padding: EdgeInsets.all(15),
               child: ExpansionTile(
                 backgroundColor: Color(0xFFFFFAF1),
@@ -98,13 +70,9 @@ class _CalendarPageState extends State<CalendarPage> {
                         fontWeight: FontWeight.bold)),
                 children: <Widget>[
                   CheckboxListTileExample(item1, item2, item3),
-                  
                 ],
               ),
             ),
-
-
-
 
             // Milestones Card
             const Padding(
