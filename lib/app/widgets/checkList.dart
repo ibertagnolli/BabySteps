eebc622d0fffaa914a2c3531ebe396c73bb352df
ibertagnolli <ibertagnolli@hotmail.com>
2024-01-14 20:47:02 -0700

    List view, waiting to see if we need this w/ DB

--- lib/app/widgets/checkList.dart
+++ lib/app/widgets/checkList.dart
@@ -40,25 +40,25 @@ class _CheckboxListTileExampleState extends State<CheckboxListTileExample> {
       children: <Widget>[
         //TODO: use a list view builder here instead of hardcoding 3 items 
         //This is causing an error right now
-        ListView.builder(
-          // Let the ListView know how many items it needs to build.
-          itemCount: items.length,
-          // Provide a builder function. This is where the magic happens.
-          // Convert each item into a widget based on the type of item it is.
-          itemBuilder: (context, index) {
-            final item = items[index];
+        // ListView.builder(
+        //   // Let the ListView know how many items it needs to build.
+        //   itemCount: items.length,
+        //   // Provide a builder function. This is where the magic happens.
+        //   // Convert each item into a widget based on the type of item it is.
+        //   itemBuilder: (context, index) {
+        //     final item = items[index];
 
-            return CheckboxListTile(
-              value: checkboxValue1,
-              onChanged: (bool? value) {
-                setState(() {
-                  checkboxValue1 = value!;
-                });
-              },
-              title: Text(item), 
-            );
-          },
-        ),
+        //     return CheckboxListTile(
+        //       value: checkboxValue1,
+        //       onChanged: (bool? value) {
+        //         setState(() {
+        //           checkboxValue1 = value!;
+        //         });
+        //       },
+        //       title: Text(item), 
+        //     );
+        //   },
+        // ),
 
         CheckboxListTile(
           value: checkboxValue1,
