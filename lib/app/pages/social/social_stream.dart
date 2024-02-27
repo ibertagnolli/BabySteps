0f5bd82220a62e28d4d86dcdcae2094dd44db33e
ibertagnolli <ibertagnolli@hotmail.com>
2024-02-26 22:19:53 -0700

    Yay it works and its pretty ish, :( repeated code

--- lib/app/pages/social/social_stream.dart
+++ lib/app/pages/social/social_stream.dart
@@ -18,67 +18,185 @@ class SocialStream extends StatefulWidget {
 class _SocialStreamState extends State<SocialStream> {
   final Stream<QuerySnapshot> _socialStream =
       SocialDatabaseMethods().getStream();
-      late var posts;
-     // late ImageProvider flutterImageProvider;
+  late var posts;
 
+  Future<pw.Document> createMultiPdf(posts) async {
+    final pw.Document pdf = pw.Document();
+    //For loop over posts and turn them into pdf widgets
+    late String userName;
+    late DateTime date;
+    late String? title;
+    late String? caption;
+    late String child;
+    late String? imagePath;
+    late Image? networkImage;
+    List<pw.Widget> widgets = [];
 
-Future<pw.Document> createMultiPdf(posts) async {
-  final pw.Document pdf = pw.Document();
-  //For loop over posts and turn them into pdf widgets 
-  //Image image = Image.file(File(imagePath));
-   late String userName;
-   late DateTime date;
-   late String? title;
-   late String? caption;
-   late String child;
-   late String? imagePath;
-   late Image? networkImage;
-   List<pw.Widget> widgets = [];
-
-   for (var post in posts) {
-             userName = post['usersName'];
-             date = post['date'].toDate();
-             title = post['title'];
-             caption = post['caption'];
-             child = post['child'];
-             imagePath = post['image'];
-             networkImage = Image.network(imagePath!);
-             //http.Response response = await http.get( Uri.parse(imagePath!));
-             Uint8List bytes = (await NetworkAssetBundle(Uri.parse(imagePath!)).load(imagePath!))
-    .buffer
-    .asUint8List();
-        // NetworkImage(imagePath.toString()))
-
+    for (var post in posts) {
+      userName = post['usersName'];
+      date = post['date'].toDate();
+      title = post['title'] ?? '';
+      caption = post['caption'] ?? '';
+      child = post['child'];
+      imagePath = post['image'];
+      //If there is no image since image is optional don't add it to pdf
+      //annoying that I have to do this twice, may be able to clean up by making a widget?
+      if (imagePath == null) {
         widgets.add(
-           pw.Padding(padding: const pw.EdgeInsets.all(16),
-            child:pw.Column(
-          children: [
-            pw.Text(userName),
-            pw.Text(date.toString()),
-            pw.Text(child),
-            pw.Text(caption!),
-            pw.Text(title!),
-            //pw.Image(networkImage as pw.ImageProvider)
-            pw.Image(pw.MemoryImage(bytes)),
-          ],
-          ),
-        ),
-      );
-   }
-    //then add pages from the widgets list
-        pdf.addPage(
-          pw.MultiPage(
-            pageFormat: PdfPageFormat.a4,
-            build: (context) => widgets,//here goes the widgets list
+          pw.Padding(
+            padding: const pw.EdgeInsets.all(16),
+            child: pw.Container(
+              width: 500,
+              height: 200,
+              decoration: pw.BoxDecoration(
+                  color: PdfColor.fromHex("#F2BB9B"),
+                  borderRadius: pw.BorderRadius.circular(20)),
+              child: pw.Column(
+                children: [
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(3),
+                    child: pw.Text(userName,
+                        style: pw.TextStyle(
+                            fontSize: 10, color: PdfColor.fromHex("#0D4B5F")),
+                        textAlign: pw.TextAlign.start),
+                  ),
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(3),
+                    child: pw.Text(child,
+                        style: pw.TextStyle(
+                            fontSize: 10, color: PdfColor.fromHex("#FFFAF1")),
+                        textAlign: pw.TextAlign.start),
+                  ),
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(3),
+                    child: pw.Text(date.toString(),
+                        style: pw.TextStyle(
+                            fontSize: 10, color: PdfColor.fromHex("#4F646F")),
+                        textAlign: pw.TextAlign.start),
+                  ),
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(6),
+                    child: pw.Text(title ?? '',
+                        style: pw.TextStyle(
+                            fontSize: 15, color: PdfColor.fromHex("#0D4B5F"))),
+                  ),
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(6),
+                    child: pw.Text(caption ?? '',
+                        style: pw.TextStyle(
+                            fontSize: 10, color: PdfColor.fromHex("#4F646F"))),
+                  ),
+                ],
+              ),
+            ),
           ),
         );
+        //Otherwise add it to the pdf
+      } else {
+        networkImage = Image.network(imagePath!);
+
+        Uint8List bytes =
+            (await NetworkAssetBundle(Uri.parse(imagePath)).load(imagePath))
+                .buffer
+                .asUint8List();
+
+        widgets.add(
+          pw.Padding(
+            padding: const pw.EdgeInsets.all(5),
+            child: pw.Container(
+              width: 500,
+              height: 200,
+              decoration: pw.BoxDecoration(
+                  color: PdfColor.fromHex("#F2BB9B"),
+                  borderRadius: pw.BorderRadius.circular(20)),
+              child: pw.Row(
+                children: [
+                  pw.Container(
+                    height: 200,
+                    width: 200,
+                    decoration: pw.BoxDecoration(
+                      image: pw.DecorationImage(
+                        image: pw.MemoryImage(bytes),
+                      ),
+                      borderRadius: const pw.BorderRadius.only(
+                          topLeft: pw.Radius.circular(10),
+                          bottomLeft: pw.Radius.circular(10)),
+                    ),
+                  ),
+                  pw.Container(
+                    width: 280,
+                    height: 200,
+                    child: pw.Padding(
+                      padding: const pw.EdgeInsets.all(10),
+                      child: pw.Column(
+                        crossAxisAlignment: pw.CrossAxisAlignment.start,
+                        children: [
+                          pw.Padding(
+                            padding: const pw.EdgeInsets.all(3),
+                            child: pw.Text(userName,
+                                style: pw.TextStyle(
+                                    fontSize: 10,
+                                    color: PdfColor.fromHex("#0D4B5F")),
+                                textAlign: pw.TextAlign.start),
+                          ),
+                          pw.Padding(
+                            padding: const pw.EdgeInsets.all(3),
+                            child: pw.Text(child,
+                                style: pw.TextStyle(
+                                    fontSize: 10,
+                                    color: PdfColor.fromHex("#FFFAF1")),
+                                textAlign: pw.TextAlign.start),
+                          ),
+                          pw.Padding(
+                            padding: const pw.EdgeInsets.all(3),
+                            child: pw.Text(date.toString(),
+                                style: pw.TextStyle(
+                                    fontSize: 10,
+                                    color: PdfColor.fromHex("#4F646F")),
+                                textAlign: pw.TextAlign.start),
+                          ),
+                          pw.Padding(
+                            padding: const pw.EdgeInsets.all(3),
+                            child: pw.Text(title ?? '',
+                                style: pw.TextStyle(
+                                    fontSize: 15,
+                                    color: PdfColor.fromHex("#0D4B5F"))),
+                          ),
+                          pw.Padding(
+                            padding: const pw.EdgeInsets.all(3),
+                            child: pw.Text(caption ?? '',
+                                style: pw.TextStyle(
+                                    fontSize: 10,
+                                    color: PdfColor.fromHex("#4F646F"))),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          //  ),
+        );
+        // }
+      }
+    }
+    //then add pages from the widgets list
+    pdf.addPage(
+      pw.MultiPage(
+        maxPages: 1000,
+        pageFormat: PdfPageFormat.a4,
+        build: (context) => widgets, //here goes the widgets list
+      ),
+    );
     final output = await getTemporaryDirectory();
+    print(output.path);
     var path = "${output.path}/test.pdf";
     final file = File(path);
     await file.writeAsBytes(await pdf.save());
     return pdf;
-}
-
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -114,18 +232,18 @@ Future<pw.Document> createMultiPdf(posts) async {
               image: imagePath,
             ));
           }
-        
         }
 
-        return Column(
-            children: [
-              Column( children: postWidgets.isNotEmpty
-                ? postWidgets
-                : [const Text("no posts")],),
-                ElevatedButton(onPressed: () => createMultiPdf(posts), child: const Text("save to pdf"))]);
+        return Column(children: [
+          Column(
+            children:
+                postWidgets.isNotEmpty ? postWidgets : [const Text("no posts")],
+          ),
+          ElevatedButton(
+              onPressed: () => createMultiPdf(posts),
+              child: const Text("save to pdf"))
+        ]);
       },
-
-
     );
   }
 }
