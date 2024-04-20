75e2e034152e438458dce162ce64698025724791
audreyOlson <audreybrynolson@gmail.com>
2024-04-20 15:52:18 -0600

    adding notifications back in done

--- lib/app/pages/calendar/Events/add_event_button.dart
+++ lib/app/pages/calendar/Events/add_event_button.dart
@@ -43,17 +43,6 @@ class _AddEventButtonState extends State<AddEventButton> {
       setState(() {
         eventTime = selectedTime;
         timeController.text = selectedTime.format(context);
-        // NotificationService().scheduleNotification(
-        //     id: 0,
-        //     title: eventName,
-        //     body:
-        //         '${selectedTime.hour.toString().padLeft(2, '0')}:${selectedTime.minute.toString().padLeft(2, '0')}',
-        //     scheduledNotificationDateTime: DateTime(
-        //         selectedDay.year,
-        //         selectedDay.month,
-        //         selectedDay.day,
-        //         selectedTime.hour,
-        //         selectedTime.minute));
       });
     }
   }
@@ -71,6 +60,18 @@ class _AddEventButtonState extends State<AddEventButton> {
     };
     await CalendarDatabaseMethods()
         .addEvent(uploaddata, currentUser.value!.userDoc);
+
+    NotificationService().scheduleNotification(
+        id: 0,
+        title: nameController.text,
+        body:
+            '${eventTime.hour.toString().padLeft(2, '0')}:${eventTime.minute.toString().padLeft(2, '0')}',
+        scheduledNotificationDateTime: DateTime(
+            widget.selectedDay.year,
+            widget.selectedDay.month,
+            widget.selectedDay.day,
+            eventTime.hour,
+            eventTime.minute));
 
     // Clear fields for next entry (not date)
     nameController.clear();
